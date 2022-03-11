082fc00b5527ab9c1c8d249bc37d8a4a2d28f90b
Ampfar10 <94465412+Ampfar10@users.noreply.github.com>
2022-03-11 18:58:10 +0200

    Update Wallet.ts

--- src/commands/Games/Wallet.ts
+++ src/commands/Games/Wallet.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
       ];
     
       const buttonMessage: any = {
-      contentText: `👛 *Wallet | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
+      contentText: `🏦 *Bank | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
       footerText: "🎇 Beyond 🎇",
       buttons: buttons,
       headerType: 1,
