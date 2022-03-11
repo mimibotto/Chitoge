f26478b49086641ebcd40c43f6a30be783408552
Ampfar10 <94465412+Ampfar10@users.noreply.github.com>
2022-03-12 00:27:22 +0200

    Update Gamble.ts

--- src/commands/Games/Gamble.ts
+++ src/commands/Games/Gamble.ts
@@ -24,7 +24,7 @@ export default class Command extends BaseCommand {
     { joined }: IParsedArgs
   ): Promise<void> => {
     /*eslint-disable @typescript-eslint/no-explicit-any*/
-    if (M.from !== "120363040180352126@g.us")
+    if (M.from !== "120363040909344977@g.us")
       return void M.reply(
         `You can't gamble here. Use ${this.client.config.prefix}support to get casino group link.`
       );
